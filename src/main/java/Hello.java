126534195cec228987ff3d5e217c9e503e998b99
yys <yys@sias.com>
2022-12-01 14:07:27 +0800

    master

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("123456789");
         System.out.println("123456789");
+        System.out.println("xcvbn");
 
     }
 }
